4b8af99ebb3dbbfc51d356ad7cac860149c6b5e1
杨博 <807909564@qq.com>
2016-07-14 11:09:43 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
